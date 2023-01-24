d2916db7ba3c16002113a8c1e6573244c6bc3fd4
gael <76548719+gaelzarco@users.noreply.github.com>
2023-01-24 14:24:18 -0800

    boilerplate user form information created

--- client/src/components/CreateAccount.tsx
+++ client/src/components/CreateAccount.tsx
@@ -1,14 +1,41 @@
 import { AiFillCloseSquare } from "react-icons/ai"
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 import { useNavigate } from "react-router-dom"
 
 function CreateAccount() {
     const navigate = useNavigate()
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        console.log('submit!')
+    const [ credentials, setCredentials ] = useState({
+        email: '',
+        username: '',
+        password: ''
+    })
+    
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      console.log(credentials)
+    
+    //   const response = await fetch('/', {
+    //     method: 'POST',
+    //     headers : {
+    //       'Content-Type': 'application/json'
+    //       },
+    //     body: JSON.stringify(credentials)
+    //   })
+    
+    //   const data = await response.json()
+    //   console.log(data)
     }
+
+    const emailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCredentials({...credentials, email: event.target.value});
+    };
+    const usernameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCredentials({...credentials, username: event.target.value});
+      };
+      const passwordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCredentials({...credentials, password: event.target.value});
+      };
 
     return (
         <div className='pop-container'>
@@ -21,10 +48,10 @@ function CreateAccount() {
 
                 <div>
                 <form onSubmit={handleSubmit}>
-                    <div><input type='email' placeholder='Email' /></div>
-                    <div><input type='text' placeholder='Username' /></div>
-                    <div><input type='password' placeholder='Password' /></div>
-                    <div><input type='button' value='submit' /></div>
+                    <div><input type='email' placeholder='Email' onChange={(e) => {emailHandler(e)}}/></div>
+                    <div><input type='text' placeholder='Username' onChange={(e) => {usernameHandler(e)}}/></div>
+                    <div><input type='password' placeholder='Password' onChange={(e) => {passwordHandler(e)}}/></div>
+                    <div><input type='submit' value='submit' /></div>
                 </form>
                 </div>
             </div>
